0683c08db741c0dc84690c428d886569c2f4d2b8
J0hn316 <jcordell316@gmail.com>
2023-10-11 22:31:42 -0400

    done

---
+++ src/components/editcardpage/EditCardBG.jsx
@@ -0,0 +1,10 @@
+import "../editcardpage/editcard.css";
+import Img from "../../../img/PokemonCards2.png";
+
+export default function EditCardBg() {
+  return (
+    <div className="EDBG">
+      <img src={Img} alt="PokemonCard Background" />
+    </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import HomeBackground from "../components/homepage/HomeBackground";
+import "../components/homepage/homepage.css";
+import { useEffect, useState } from "react";
+import { getCards } from "../../services/Api";
+import { useNavigate } from "react-router-dom";
+
+function Home() {
+  const [cards, setCards] = useState([]);
+  const nav = useNavigate();
+
+  useEffect(() => {
+    getCards().then((res) => setCards(res.data));
+  }, []);
+
+  return (
+    <div className="Home">
+      <HomeBackground />
+      <h1>Welcome to HomePage</h1>
+      <ul className="container">
+        {cards.map((card) => {
+          return (
+            <div>
+              <div className="pokeimg">
+                <h3>{`${card?.name}`}</h3>
+                <img src={card.img + ".jpg"} />
+                <button
+                  onClick={() => {
+                    nav(`/cards/edit/${card._id}`);
+                  }}
+                >
+                  Edit
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
